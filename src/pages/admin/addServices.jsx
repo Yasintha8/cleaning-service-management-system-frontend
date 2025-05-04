90a8d88b6dba7f2e76d0e25ea1107356543cb6b7
Yasintha28 <yasinthachamikara28@gmail.com>
2025-05-05 00:22:08 +0530

    add service description

--- src/pages/admin/addServices.jsx
+++ src/pages/admin/addServices.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 export default function AddProductForm() {
 
     const[name, setName] = useState("");
+    const[description, setDescription] = useState("");
 
     const navigate = useNavigate();
 
@@ -15,6 +16,7 @@ export default function AddProductForm() {
         try{        
         const service = {
             name: name,
+            description: description
         }
         const token = localStorage.getItem("token")
         console.log(token);
@@ -46,6 +48,16 @@ export default function AddProductForm() {
                 className="w-[400px] h-[50px] border border-gray-500 text-gray placeholder-gray rounded-xl p-4 m-[5px] focus:outline-none focus:ring-1 focus:ring-white"
                 placeholder="Service Name"
             />
+
+            <input 
+                value={description}
+                onChange={
+                    (e) => 
+                        setDescription(e.target.value)
+                }
+                className="w-[400px] h-[50px] border border-gray-500 text-gray placeholder-gray rounded-xl p-4 m-[5px] focus:outline-none focus:ring-1 focus:ring-white"
+                placeholder="Service Description"
+            />          
             <div className="w-[400px] h-[100px] flex justify-between items-center rounded-lg">
                 <Link to={"/admin/services"} className="bg-red-400 text-white text-xl p-[10px] w-[180px] text-center rounded-lg hover:bg-red-500  cursor-pointer ">Cancle</Link>
                 <button onClick={handleSubmit} className="bg-green-400 text-white text-xl p-[10px] w-[180px] text-center rounded-lg ml-[10px] hover:bg-green-500  cursor-pointer ">Add Product</button>
